556665334dc2963bef206f82a75a973ad7fd0f00
daypham <80749534+daypham@users.noreply.github.com>
2022-08-20 15:27:49 +0700

    Add files via upload

---
+++ wishlist.php
@@ -0,0 +1,67 @@
+<?php
+	include('inc/header.php');
+?>
+<?php
+	if(isset($_GET['proId'])){
+		$customer_id = Session::get('customer_id');
+        $id = $_GET['proId'];
+        $del_wishlist = $pro->del_wishlist($customer_id,$id);
+    } 
+?>
+<div class="main">
+    <div class="content">
+    	<div class="cartoption">		
+			<div class="cartpage">
+			    	<h2>Danh sách yêu thích</h2>
+			    
+						<table class="tblone">
+							<tr>
+								<th width="10%">ID Compare</th>
+								<th width="20%">Product Name</th>
+								<th width="20%">Image</th>
+								<th width="15%">Price</th>
+								<th width="15%">View</th>
+								<th width="25%">Action</th>
+
+
+							</tr>
+							<?php
+								$customer_id = Session::get('customer_id');
+								$get_wishlist = $pro->get_wishlist($customer_id);
+								if($get_wishlist){
+									$i = 0;
+									while($result = $get_wishlist->fetch_array()){
+										$i++;
+							?>
+							<tr>
+								<td><?php echo $i?></td>
+								<td><?php echo $result['productName']?></td>
+								<td><img src="admin/uploads/<?php echo $result['img']?>" alt=""/></td>
+								<td><?php echo $result['price']." VND"?></td>
+								<td><a href="details.php?proId=<?php echo $result['productId']?>">View</a></td>
+								<form action="" method="post">
+
+									<td><a href="details.php?proId=<?php echo $result['productId']?>">Buy</a> ||
+										<a onclick = "return confirm('Bạn có chắc muốn xóa?')" href="?proId=<?php echo $result['productId']?>">Delete</a></td>
+								</form>
+								
+							</tr>
+							<?php
+							}
+							} 
+							?>
+							
+						</table>
+					</div>
+					<div class="shopping">
+						<div class="shopleft">
+							<a href="index.php"> <img src="images/shop.png" alt="" /></a>
+						</div>
+					</div>
+    	</div>  	
+       <div class="clear"></div>
+    </div>
+ </div>
+ <?php
+	include('inc/footer.php');
+?>
